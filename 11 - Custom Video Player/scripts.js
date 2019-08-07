9b4314fb93dad00d9cf919aee9890713957e39f8
Casey Brannon <cbrannon412@gmail.com>
2019-08-07 14:07:01 -0700

    11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,62 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.player__button2')
+
+
+function togglePay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon
+}
+
+function skip() {
+    console.log('skiping')
+    video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.value)
+    console.log(this.name)
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+    const scrubTie = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTie;
+    console.log(e)
+}
+
+function handleFullScreen(e) {
+    console.log('Hiiiting')
+    console.log(e)
+}
+
+video.addEventListener('click', togglePay)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+video.addEventListener('timeupdate', handleProgress)
+toggle.addEventListener('click', togglePay)
+skipButtons.forEach(button => button.addEventListener('click', skip))
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+let mousedown = false;
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', scrub)
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener('mousemove', () => mousedown = true)
+progress.addEventListener('mousemove', () => mousedown = false)
+fullScreen.addEventListener('click', handleFullScreen)
